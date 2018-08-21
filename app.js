22fed1c195ec4da2f815584ab348e0e59f121905
nytheneals <nythes@gmail.com>
2018-08-21 09:30:01 -0400

    [Addition] - added addition functionality

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const books = document.querySelector('#book-list ul');
-const hideCheck = document.forms['add-book'];
+const addBook = document.forms['add-book'];
+
 console.log(books.parentElement.parentElement);
 console.log(books.children);
 console.log(books.nextElementSibling);
 console.log(books.previousElementSibling);
 // ADD BOOK
+const input = addBook.querySelector('input[type="text"]');
+console.log(input);
+addBook.addEventListener('submit', e => {
+  e.preventDefault();
+  const inputValue = input.value;
+  input.value = '';
+  // NEW ELEMENTS
+  const liName = document.createElement('span');
+  const liButton = document.createElement('span');
+  const li = document.createElement('li');
+  liName.classList.add('name');
+  liButton.classList.add('delete');
+  li.appendChild(liName);
+  li.appendChild(liButton);
+  liName.textContent = inputValue;
+  liButton.textContent = 'delete';
+  console.log(inputValue);
+  console.log(li);
+  books.appendChild(li);
+});
 // DELETE BOOK
 books.addEventListener('click', e => {
   if (e.target.className === 'delete') {
@@ -14,8 +35,8 @@ books.addEventListener('click', e => {
 
 // UPDATE BOOK
 // HIDE OR SHOW BUTTON
-const radioButton = hideCheck.querySelector('input[type="checkbox"]');
-const label = hideCheck.querySelector('label');
+const radioButton = addBook.querySelector('input[type="checkbox"]');
+const label = addBook.querySelector('label');
 radioButton.addEventListener('change', () => {
   if (radioButton.checked) {
     books.style.display = 'none';
